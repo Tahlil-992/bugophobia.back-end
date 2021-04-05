e30ad8ff318ec20fde6c14dec2b7658d38c12344
rajabi <mohammad.h.rajabi.g78@gmail.com>
2021-04-05 17:26:42 +0430

    doctor_model

--- bugophobia_back/users/models.py
+++ bugophobia_back/users/models.py
@@ -12,3 +12,14 @@ class Patient(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Doctor(AbstractUser):
+    email = models.EmailField(max_length=255 , unique=True)
+    username = models.CharField(max_length=255 , unique= True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+    
