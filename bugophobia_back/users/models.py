8b29c03529ccea4df5e1f5964039c2a6869ad25c
unknown <sinaeskandari007@gmail.com>
2021-04-13 21:14:39 +0430

    added primary key to patient and doctor

--- bugophobia_back/users/models.py
+++ bugophobia_back/users/models.py
@@ -10,7 +10,7 @@ class BaseUser(AbstractUser):
     GENDERS = [('M', 'Male'),
                ('F', 'Female')]
 
-    pro_picture = models.ImageField(null = True , blank = True)
+    pro_picture = models.ImageField(null=True, blank=True)
     email = models.EmailField(max_length=255, unique=True)
     username = models.CharField(max_length=255, unique=True)
     first_name = models.CharField(max_length=255, null=True, blank=True)
@@ -32,7 +32,7 @@ class Patient(models.Model):
                        ('H', 'havades'),
                        ('T', 'takmili')]
 
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
     insurance_type = models.CharField(max_length=255, choices=INSURANCE_TYPES, default='O', null=True, blank=True)
 
     def __str__(self):
@@ -55,11 +55,11 @@ class Doctor(models.Model):
         ('U', 'Urologist'),
     ]
 
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
     filed_of_specialization = models.CharField(max_length=255, choices=FILED_OF_SPECIALIZATION, default='G', null=False,
                                                blank=False)
     gmc_number = models.IntegerField(null=False)
-    work_experience = models.IntegerField(default=0 , null=False)
+    work_experience = models.IntegerField(default=0, null=False)
 
     def __str__(self):
         return self.user.email
