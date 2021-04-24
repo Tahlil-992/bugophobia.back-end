71768551afd46e93fafe6f67dea6a75c5b5f192d
rajabi <mohammad.h.rajabi.g78@gmail.com>
2021-04-24 10:45:28 +0430

    do

--- bugophobia_back/users/serializers.py
+++ bugophobia_back/users/serializers.py
@@ -7,105 +7,42 @@ from .models import *
 
 
 class RegisterBaseUserSerializer(serializers.ModelSerializer):
-
-
     class Meta:
-
-
         model = BaseUser
-
-
         fields = ['pro_picture','email', 'username', 'first_name', 'last_name', 'gender', 'age', 'phone_number', 'city', 'is_doctor',
-
-
                   'password']
-
-
         extra_kwargs = {'password': {'write_only': True}}
 
-
-
     def create(self, validated_data):
-
-
         password = validated_data.pop('password', None)
-
-
         instance = self.Meta.model(**validated_data)
-
-
         if password is not None:
-
-
             instance.set_password(password)
-
-
         instance.save()
         return instance
 
 
-
-
 # patient
 
-
-
 class PatientDetailSerializer(serializers.ModelSerializer):
-
-
     user = RegisterBaseUserSerializer()
-
-
-
     class Meta:
-
-
         model = Patient
-
-
         fields = ['user', 'insurance_type']
 
 
-
-
 class RegisterPatientSerializer(serializers.ModelSerializer):
-
-
     user = RegisterBaseUserSerializer()
-
-
-
     class Meta:
-
-
         model = Patient
-
-
         fields = ('user', 'insurance_type')
-
-
-
     def create(self, validated_data):
-
-
         user_data = validated_data.pop('user')
-
-
         password = user_data['password']
-
-
         user = BaseUser(**user_data)
-
-
         if password is not None:
-
-
             user.set_password(password)
-
-
         user.save()
-
-
         patient = Patient.objects.create(user=user, **validated_data)
         return patient
 
@@ -117,15 +54,8 @@ class RegisterPatientSerializer(serializers.ModelSerializer):
 
 
 class DoctorDetailSerializer(serializers.ModelSerializer):
-
-
     user = RegisterBaseUserSerializer()
-
-
-
     class Meta:
-
-
         model = Doctor
         fields = ['user', 'gmc_number', 'filed_of_specialization', 'work_experience']
 
@@ -133,12 +63,7 @@ class DoctorDetailSerializer(serializers.ModelSerializer):
 
 
 class RegisterDoctorSerializer(serializers.ModelSerializer):
-
-
     user = RegisterBaseUserSerializer()
-
-
-
     class Meta:
         model = Doctor
         fields = ('user', 'gmc_number', 'filed_of_specialization' , 'work_experience')
