33055b64300ef07d546edd0b5485844ae650c4b3
unknown <sinaeskandari007@gmail.com>
2021-05-03 17:20:33 +0430

    added search all and limited search with username and name

--- bugophobia_back/search/urls.py
+++ bugophobia_back/search/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<username>/', views.SearchView.as_view()),
+    path('all/<q>/', views.SearchAllDoctorsView.as_view()),
+    path('limited/<q>/', views.LimitedSearchDoctorsView.as_view()),
 ]
--- bugophobia_back/search/views.py
+++ bugophobia_back/search/views.py
@@ -1,16 +1,40 @@
-from django.shortcuts import render
+from django.db.models import Q
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from users.models import *
 from user_profile.serializers import ListDoctorsSerializer
+from .paginations import SearchDoctorsPagination
 
 
 # Create your views here.
 
-class SearchView(generics.ListAPIView):
+class SearchAllDoctorsView(generics.ListAPIView):
     """Search for doctors with username"""
     permission_classes = [IsAuthenticated]
     serializer_class = ListDoctorsSerializer
+    pagination_class = SearchDoctorsPagination
 
     def get_queryset(self):
-        return Doctor.objects.filter(user__username__icontains=self.kwargs.get('username'))
+        q = self.kwargs.get('q').split()
+        if len(q) == 1:  # query = first name or last name or username
+            return Doctor.objects.filter(Q(user__username__startswith=q[0]) | Q(user__first_name__startswith=q[0]) | Q(
+                user__last_name__startswith=q[0])).order_by('-user__date_joined')
+        elif len(q) > 1:  # query = first name + last name
+            return Doctor.objects.filter(user__first_name=q[0], user__last_name__startswith=q[1]).order_by(
+                '-user__date_joined')
+
+
+class LimitedSearchDoctorsView(generics.ListAPIView):
+    """Search for only 5 doctors"""
+    permission_classes = [IsAuthenticated]
+    serializer_class = ListDoctorsSerializer
+    pagination_class = SearchDoctorsPagination
+
+    def get_queryset(self):
+        q = self.kwargs.get('q').split()
+        if len(q) == 1:  # query = first name or last name or username
+            return Doctor.objects.filter(Q(user__username__startswith=q[0]) | Q(user__first_name__startswith=q[0]) | Q(
+                user__last_name__startswith=q[0])).order_by('-user__date_joined')[:5]
+        elif len(q) > 1:  # query = first name + last name
+            return Doctor.objects.filter(user__first_name=q[0], user__last_name__startswith=q[1]).order_by(
+                '-user__date_joined')[:5]
