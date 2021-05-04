c56f26fcbecd21c43532501a0a81c31ea39cc582
unknown <sinaeskandari007@gmail.com>
2021-05-04 21:21:51 +0430

    search with filters

--- bugophobia_back/search/urls.py
+++ bugophobia_back/search/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('all/<q>/', views.SearchAllDoctorsView.as_view()),
-    path('limited/<q>/', views.LimitedSearchDoctorsView.as_view()),
+    path('all/', views.SearchAllDoctorsView.as_view()),
+    path('limited/', views.LimitedSearchDoctorsView.as_view()),
 ]
